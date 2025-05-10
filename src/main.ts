95a842e6ac4ba15a1d7860ac21efc0906a674db0
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2025-05-10 16:59:16 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,26 @@ import { CustomSerializerDeserializer } from './custom.serializer';
 //api gateway tüm client isteklerini için tek bir giriş noktası olarak görev yapan bir sunucudur.
 //clientlar doğrudan microservislerlere bağlanmak yerine api gatewaye bağlanır api gateway daha sonra bu istekleri
 //uygun microservislere yönlendirir ve yanıtları toplar clienta geri gönderir.
+//api gateway olmadan yapılan işlemler meslea şöyle olurdu :
+//Client A -----> microservice 1
+//Client A -----> microservice 2
+
+//client b -----> microservice 2
+//client b -----> microservice 5
+
+//api gateway ile yapılan işlemler ise şöyle olurdu teorik olarak bakarsak :
+//Client a -----> API gateway -----> microservice 1
+//                            -----> mciroservice 2
+//                            -----> microservice 3
+
+//Client b -----> API gateway -----> microservice 2
+//                            -----> microservice 5
+
+//not : clientlar sadece api gatewayin adresini bilirler ve gateway gelen isteği alır hangi microservise yönlendirceğine karar verir kısaca.
+//single point of entry : clientların tüm microservislerin adreslerini bilmesine gerek kalmaz sadece gatewayin adresini bilmeleri yeterlidir
+//request routing : gateway gelen isteğin urline http methoduna veya headera bakarak isteği doğru microservise yönlendirir
+//mseela users ile ilgili istekler varsa User microservisine yönlendirir
+//orders ile alakalı bişey varsa Order microservisine yönlendirir
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
